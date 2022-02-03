fd093400e38cbeffd2ec1daf83bfe96e6bf01c8c
Isaque Ferreira <isaqueferreiraj@gmail.com>
2022-02-02 23:33:57 -0300

    verificador de palíndromo

--- Variáveis e tipos/verificador-de-palindromo/palindromo.js
+++ Variáveis e tipos/verificador-de-palindromo/palindromo.js
@@ -1,5 +1,21 @@
 function verificaPalindromo(string){
-    if(!string) return "String inexistente";
+    if(!string){
+        return "String inexistente";
+    } else {
+        for(var i = 0; i < string.length/2; i++){
+            console.log(string[i]);
+            if(string[i] !== string[string.length-1-i]){
+                return "Não é um palindromo";
+            }
+        }
+        return "É um palíndromo"
+    }
+}
 
+function verificaPalindromo2(string){
+    if(!string){
+        return "String inexistente";
+    }
+    
     return string.split("").reverse().join("") === string;
 }
